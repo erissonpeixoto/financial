c0ad6fdfe0aa88984bb06c0d35a8acc8ca81f975
Erisson Peixoto <erisson.peixoto@rdstation.com>
2025-03-22 10:17:18 -0300

    add tailwindcss and config authentication views

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,12 +8,17 @@ class RegistrationsController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      start_new_session_for(@user)
-      redirect_to root_path, notice: 'Usuário registrado com sucesso.'
-    else
-      flash[:alert] = @user.errors.full_messages.join(", ")
-      render :new
+    begin
+      if @user.save
+        start_new_session_for(@user)
+        redirect_to root_path, notice: "Usuário registrado com sucesso."
+      else
+        flash[:alert] = @user.errors.full_messages.join(", ")
+        render :new
+      end
+    rescue ActiveRecord::RecordNotUnique
+      flash.now[:alert] = "Este e-mail já está em uso. Escolha outro."
+      render :new, status: :unprocessable_entity
     end
   end
 
