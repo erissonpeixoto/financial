973dd93a6277cae3efb1005d32547db1d2963da7
Erisson Peixoto <erisson.peixoto@rdstation.com>
2025-04-29 17:52:07 -0300

    Config rails authorization

--- app/javascript/components/index.jsx
+++ app/javascript/components/index.jsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 document.addEventListener('turbo:load', () => {
-  const root = createRoot(
-    document.body.appendChild(document.createElement('div'))
-  );
-  root.render(<App />);
+  const rootElement = document.getElementById('root');
+  if (rootElement) {
+    const root = createRoot(rootElement);
+    root.render(<App />);
+  }
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get "home/index"
+  resource :session
+  resources :passwords, param: :token
+  resource :registration, only: [ :new, :create ]
 
   get "up" => "rails/health#show", as: :rails_health_check
-
-  get "*path", to: "home#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  # get "*path", to: "home#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
   root "home#index"
 end
